211f2d9cdf29043f7f2a9a701a9a64db1840e6f2
Xin Tan <mactanxin@gmail.com>
2021-03-25 14:14:47 +0800

    index layout

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -1,5 +1,9 @@
 <script>
+	import Header from './../components/Header.svelte';
 	import "../global.css";
+	import Footer from './../components/Footer.svelte';
 </script>
 
+<Header></Header>
 <slot />
+<Footer></Footer>
