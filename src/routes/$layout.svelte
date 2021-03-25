e8469344b2dc97da9da56ea8d5ef7191c4f35b9c
Xin Tan <mactanxin@gmail.com>
2021-03-25 18:27:42 +0800

    index layout

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -2,6 +2,10 @@
 	import Header from './../components/Header.svelte';
 	import "../global.css";
 	import Footer from './../components/Footer.svelte';
+	import resolveConfig from 'tailwindcss/resolveConfig';
+	// import * as tailwindConfig from '../../tailwind.config.cjs';
+	// const fullConfig = resolveConfig(tailwindConfig);
+
 </script>
 
 <Header></Header>
