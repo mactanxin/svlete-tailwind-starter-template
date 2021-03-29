c2dc66ec70714e57b3f844603c341632bb97b74a
Xin Tan <mactanxin@gmail.com>
2021-03-29 18:37:40 +0800

    added: store

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -2,8 +2,8 @@
 	import Header from './../components/Header.svelte';
 	import "../global.css";
 	import Footer from './../components/Footer.svelte';
-	import resolveConfig from 'tailwindcss/resolveConfig';
-	// import * as tailwindConfig from '../../tailwind.config.cjs';
+	// import resolveConfig from 'tailwindcss/resolveConfig';
+	// import * as tailwindConfig from '../../tailwind.config';
 	// const fullConfig = resolveConfig(tailwindConfig);
 
 </script>
@@ -11,3 +11,15 @@
 <Header></Header>
 <slot />
 <Footer></Footer>
+
+<style>
+	:global(body) {
+		background-color: #f5f6f6;
+		color: #0084f6;
+		transition: background-color 0.3s
+	}
+	:global(body.dark-mode) {
+		background-color: #1d3040;
+		color: #bfc2c7;
+	}
+</style>
