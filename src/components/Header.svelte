a8d99498c9429d146d64b2a4e7f6300e63d20a76
Xin Tan <mactanxin@gmail.com>
2021-03-29 18:36:23 +0800

    toggle dark theme, value from store

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,3 +1,31 @@
+<script>
+  export let segment;
+  import { theme } from '../routes/store';
+
+  const toggleMenu = () => {
+    // const menu = document.getElementById('mobile-menu');
+    // menu.classList.toggle('hidden')
+    $theme = $theme === 'dark' ? 'light' : 'dark';
+    document.querySelector('html').classList.toggle('dark')
+  }
+
+  const toggleDropdown = () => {
+    const dropdown = document.getElementById('dropdown');
+    dropdown.classList.toggle('hidden')
+  }
+
+  const toggleTheme = () => {
+    // window.document.body.classList.toggle('dark-mode')
+    // console.log('toggle')
+    $theme = $theme === 'dark' ? 'light' : 'dark';
+    document.querySelector('html').classList.toggle('dark')
+  }
+
+  let defaultMenuStyle = `text-gray-300 hover:bg-gray-700 hover:text-white`;
+  let selectedMenuStyle = `bg-gray-900 text-white`;
+  $: path = $theme === 'dark' ? `M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z` : `M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z`
+</script>
+
 <!-- This example requires Tailwind CSS v2.0+ -->
 <nav class="bg-gray-800">
   <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
@@ -36,17 +64,23 @@
         <div class="hidden sm:block sm:ml-6">
           <div class="flex space-x-4">
             <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-            <a href="#" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Dashboard</a>
+            <a href="." on:click={() => segment = undefined} class="{segment === undefined ? selectedMenuStyle : defaultMenuStyle} px-3 py-2 rounded-md text-sm font-medium" aria-current="{segment === undefined ? 'page' : undefined}">Dashboard</a>
 
-            <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Team</a>
+            <a href="team" on:click={() => segment = 'team'} class="{segment === 'team' ? selectedMenuStyle : defaultMenuStyle} px-3 py-2 rounded-md text-sm font-medium" class:current={segment === 'team'} aria-current="{segment === 'team' ? 'page' : undefined}">Team</a>
 
-            <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Projects</a>
+            <a href="projects" on:click={() => segment = 'projects'} class="{segment === 'projects' ? selectedMenuStyle : defaultMenuStyle} px-3 py-2 rounded-md text-sm font-medium"  aria-current="{segment === 'projects' ? 'page' : undefined}">Projects</a>
 
-            <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Calendar</a>
+            <a href="other" on:click={() => segment = 'other'} class="{segment === 'other' ? selectedMenuStyle : defaultMenuStyle} px-3 py-2 rounded-md text-sm font-medium" aria-current="{segment === 'other' ? 'page' : undefined}">Calendar</a>
           </div>
         </div>
       </div>
       <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+        <button on:click={toggleTheme} class="mr-2 bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+          <span class="sr-only">Toggle theme</span>
+          <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d={path}></path>
+          </svg>
+        </button>
         <button class="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
           <span class="sr-only">View notifications</span>
           <!-- Heroicon name: outline/bell -->
@@ -98,15 +132,3 @@
     </div>
   </div>
 </nav>
-
-<script>
-  const toggleMenu = () => {
-    const menu = document.getElementById('mobile-menu');
-    menu.classList.toggle('hidden')
-  }
-
-  const toggleDropdown = () => {
-    const dropdown = document.getElementById('dropdown');
-    dropdown.classList.toggle('hidden')
-  }
-</script>
