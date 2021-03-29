03004b8671a08f81ff0b6446ff2bd14f4b857040
Xin Tan <mactanxin@gmail.com>
2021-03-29 18:37:24 +0800

    ADDED: some demo code showing dark class elements

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,12 +7,12 @@
       <div class="p-4 md:w-1/3">
         <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
           <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="https://dummyimage.com/720x400" alt="blog">
-          <div class="p-6">
-            <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2>
-            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">The Catalyzer</h1>
-            <p class="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+          <div class="p-6 dark:bg-gray-800">
+            <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1 dark:text-white">CATEGORY</h2>
+            <h1 class="title-font text-lg font-medium text-gray-900 mb-3 dark:text-white">The Catalyzer</h1>
+            <p class="leading-relaxed mb-3 dark:text-gray-400">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
             <div class="flex items-center flex-wrap ">
-              <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
+              <a href="." class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
                 <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
                   <path d="M5 12h14"></path>
                   <path d="M12 5l7 7-7 7"></path>
