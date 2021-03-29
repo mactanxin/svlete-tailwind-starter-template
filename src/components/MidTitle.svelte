578d4fdc4044f4beaa2b26361fc6ecadfd503115
Xin Tan <mactanxin@gmail.com>
2021-03-29 18:38:28 +0800

    ADDED: sample code blocks from tailblock

---
+++ src/components/MidTitle.svelte
@@ -0,0 +1,10 @@
+<script>
+  export let title;
+  export let desc;
+</script>
+
+<div class="flex flex-col text-center w-full mt-10">
+  <h2 class="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">ROOF PARTY POLAROID</h2>
+  <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">{ title }</h1>
+  <p class="lg:w-2/3 mx-auto leading-relaxed text-base">{desc}</p>
+</div>
