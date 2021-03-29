72f7445795bf2c8bec2237d321f5f596585f9e6f
Xin Tan <mactanxin@gmail.com>
2021-03-29 18:38:41 +0800

    added: fake routes

---
+++ src/routes/team/index.svelte
@@ -0,0 +1,18 @@
+<script>
+	import BlogList from '../../components/BlogList.svelte';
+  let articles = [
+    {
+      category: 'JS',
+      createdAt: '2021-03-22',
+      title: 'How to TailwindCSS with Svelte',
+      content: ''
+    },
+    {
+      category: 'Svelte',
+      createdAt: '2021-03-23',
+      title: 'How to TailwindCSS with NodeJS',
+      content: ''
+    }]
+</script>
+
+<BlogList articles={articles}/>
