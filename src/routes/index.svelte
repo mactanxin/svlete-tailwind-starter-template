ef97420d1ffbe7cce57321c74de122e474632c95
Tan Xin <mactanxin@gmail.com>
2021-04-01 18:28:43 +0800

    added: theme color

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,5 @@
-<script lang="ts">
+<script>
+  import { color } from './store';
 </script>
 
 <section class="text-gray-600 body-font">
@@ -12,7 +13,7 @@
             <h1 class="title-font text-lg font-medium text-gray-900 mb-3 dark:text-white">The Catalyzer</h1>
             <p class="leading-relaxed mb-3 dark:text-gray-400">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
             <div class="flex items-center flex-wrap ">
-              <a href="." class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
+              <a href="." class="text-{$color}-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
                 <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
                   <path d="M5 12h14"></path>
                   <path d="M12 5l7 7-7 7"></path>
@@ -36,12 +37,12 @@
       <div class="p-4 md:w-1/3">
         <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
           <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="https://dummyimage.com/721x401" alt="blog">
-          <div class="p-6">
-            <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2>
-            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">The 400 Blows</h1>
+          <div class="p-6 dark:bg-gray-800">
+            <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1 dark:text-white">CATEGORY</h2>
+            <h1 class="title-font text-lg font-medium text-gray-900 mb-3 dark:text-white">The 400 Blows</h1>
             <p class="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
             <div class="flex items-center flex-wrap">
-              <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
+              <a class="text-{$color}-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
                 <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
                   <path d="M5 12h14"></path>
                   <path d="M12 5l7 7-7 7"></path>
@@ -65,12 +66,12 @@
       <div class="p-4 md:w-1/3">
         <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
           <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="https://dummyimage.com/722x402" alt="blog">
-          <div class="p-6">
+          <div class="p-6 dark:bg-gray-800">
             <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2>
-            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Shooting Stars</h1>
+            <h1 class="title-font text-lg font-medium text-gray-900 mb-3 dark:text-white">Shooting Stars</h1>
             <p class="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
             <div class="flex items-center flex-wrap ">
-              <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
+              <a class="text-{$color}-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
                 <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
                   <path d="M5 12h14"></path>
                   <path d="M12 5l7 7-7 7"></path>
